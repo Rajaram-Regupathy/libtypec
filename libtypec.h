5687ea5a02b0138e13230409a2b1699ac6b9460d
Rajaram Regupathy <rajaram.regupathy@gmail.com>
2022-01-24 18:18:57 +0530

    libtypec: add proper license and create LICENSES folder
    This commit aligns the library with proper license which is
    with Linux and other Linux usb utilities license expectation
    and compatability

--- libtypec.h
+++ libtypec.h
@@ -1,21 +1,17 @@
 /*
-This file is part of libtypec.
+    Copyright (c) 2021-2022 by Rajaram Regupathy, rajaram.regupathy@gmail.com
 
-libtypec  is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-any later version.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; version 2 of the License.
 
-libtypec is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with libtypec that can be found in the LICENSE file.  
-If not, see <http://www.gnu.org/licenses/>.
+    (See the full license text in the LICENSES directory)
 */
-// SPDX-License-Identifier: GPL-3.0 or later
+// SPDX-License-Identifier: GPL-2.0-only
 /**
  * @file libtypec.h
  * @author Rajaram Regupathy <rajaram.regupathy@gmail.com>
